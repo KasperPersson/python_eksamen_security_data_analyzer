c58d2031c82079854e9842e48433e6d6f5642aca
KasperPersson <kape0010@stud.kea.dk>
2026-05-28 11:22:33 +0200

    Changed textfield on app.py and added .ds_store to gitignore for mac users

--- frontend/app.py
+++ frontend/app.py
@@ -9,8 +9,8 @@ st.set_page_config(
 st.title(":rainbow[Security Data Analyzer]")
 st.write(
     "A cybersecurity application that lets you explore ~6 million network log entries "
-    "from a real dataset. "
-    "Use it to analyze threat patterns, suspicious IP's "
+    "from a real dataset.  \n"
+    "Use it to analyze threat patterns, suspicious IP's  \n"
     "Or use the AI function to generate an assesment and chat about the data "
 )
 st.info("Select a page from the sidebar to begin.")
